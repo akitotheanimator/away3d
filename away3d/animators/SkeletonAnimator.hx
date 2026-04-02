759688333a3f439cfcaf3acbc8f1076c16083937
akitotheanimator <sapressaonenem@gmail.com>
2026-04-02 17:40:35 -0300

    Update SkeletonAnimator.hx

--- away3d/animators/SkeletonAnimator.hx
+++ away3d/animators/SkeletonAnimator.hx
@@ -284,7 +284,9 @@ class SkeletonAnimator extends AnimatorBase implements IAnimator {
 		        basePose.translation.z += addPose.translation.z * _additiveWeight;
 		
 		        // rotation additive
-		        basePose.orientation = Quaternion.slerp(basePose.orientation, addPose.orientation, _additiveWeight);
+		        var blended:Quaternion = new Quaternion();
+		        blended.slerp(basePose.orientation, addPose.orientation, _additiveWeight);
+		        basePose.orientation = blended;
 		    }
 		}
 
